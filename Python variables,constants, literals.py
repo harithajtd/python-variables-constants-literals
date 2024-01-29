fbb582dca91c49de11f19780ba7e0023854a0b05
harithajtd <148217601+harithajtd@users.noreply.github.com>
2024-01-29 13:48:23 +0530

    Add files via upload

---
+++ Python variables,constants, literals.py
@@ -0,0 +1,93 @@
+#variable
+
+number=10
+name="Haritha"
+temperature=98.6
+
+#assigning values to variables
+
+name="Haritha"
+
+print(name)
+
+
+"""__________________________________________________________"""
+
+#changing the value of a variable
+
+name="Gowthami"
+print(name)
+#Assigning new value to name
+name="Subbu"
+print(name)
+
+"""__________________________________________________________"""
+
+
+#assigning multiple values to multiple variables
+
+a,b,c="Hari",5,8.7
+print(a)
+print(b)
+print(c)
+
+"""__________________________________________________________"""
+
+
+#assigning same value to different variables
+
+marks=temperature=98.6
+print(marks)
+print(temperature)
+
+"""__________________________________________________________"""
+
+
+#declaring constants after creating constants.py file
+
+#PI=3.14159
+#GRAVITY=9.8
+
+#importing constants file we created before
+
+import constants
+print(constants.PI)
+print(constants.GRAVITY)
+
+"""___________________________________________________________"""
+
+#LITERALS in python
+name="Haritha"
+
+#BOOLEAN Literals
+a=True
+b=False
+
+#string and charcater literal
+string="Haritha is a good girl"
+character="H"
+
+#SPECIAL Literal
+special_lit=None
+print(special_lit)
+
+#literal collections
+#1.List literal
+names=["Haritha","Gowthami","Hema"]
+print(names)
+
+#2.Tuple literal
+marks=(21,22,19,23)
+print(marks)
+
+#3.Dictionary literal
+person_fav_fruit={"Hari":"Apple","Gowtham":"Banana","Hema":"Grapes"}
+print(person_fav_fruit)
+
+#4.Set literal
+vowels_set={"a","e","i","o","u"}
+print(vowels_set)
+
+
+
+      
